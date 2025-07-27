5181ad0e4c1ef1a39b072b3b5a06bd80662419be
FlyAwake <3085384553@qq.com>
2025-07-27 14:42:00 +0800

    路由改写

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -5,6 +5,8 @@ import Register from '../views/user/Register';
 import Profile from '../views/user/Profile';
 import Admin from '../views/admin/Admin';
 import Create from '../views/admin/Create';
+import ActivityList from '../views/activity/ActivityList';
+import ActivityDetail from '../views/activity/ActivityDetail';
 
 const routes = [
   { path: '/', element: <Login />, name: '登录' },
@@ -12,6 +14,8 @@ const routes = [
   { path: '/profile', element: <Profile />, name: '个人信息' },
   { path: '/admin', element: <Admin />, name: '管理员界面' },
   { path: '/create', element: <Create />, name: '创建活动' },
+  { path: '/activities', element: <ActivityList />, name: '活动列表' },
+  { path: '/activity/:id', element: <ActivityDetail />, name: '活动详情' },
 ];
 
 const AppRouter = () => {
